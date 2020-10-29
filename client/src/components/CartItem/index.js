03868b33974a3dd223b7cf8211f6acf8aa5fe515
carolina <carolinaccez@gmail.com>
2020-10-29 17:47:06 -0400

    offline feat.

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useStoreContext } from '../../utils/GlobalState';
 import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
+import { idbPromise } from "../../utils/helpers";
 
 const CartItem = ({ item }) => {
 
@@ -11,6 +12,7 @@ const CartItem = ({ item }) => {
             type: REMOVE_FROM_CART,
             _id: item._id
         });
+        idbPromise('cart', 'delete', { ...item });
     };
 
     const onChange = (e) => {
@@ -21,44 +23,48 @@ const CartItem = ({ item }) => {
                 type: REMOVE_FROM_CART,
                 _id: item._id
             });
+
+            idbPromise('cart', 'delete', { ...item });
         } else {
             dispatch({
                 type: UPDATE_CART_QUANTITY,
                 _id: item._id,
                 purchaseQuantity: parseInt(value)
             });
-        }
-    };
 
-    return (
-        <div className="flex-row">
-            <div>
-                <img
-                    src={`/images/${item.image}`}
-                    alt=""
-                />
-            </div>
-            <div>
-                <div>{item.name}, ${item.price}</div>
+            idbPromise('cart', 'put', { ...item, purchaseQuantity: parseInt(value) });
+        }
+    }
+
+        return (
+            <div className="flex-row">
                 <div>
-                    <span>Qty:</span>
-                    <input
-                        type="number"
-                        placeholder="1"
-                        value={item.purchaseQuantity}
-                        onChange={onChange}
+                    <img
+                        src={`/images/${item.image}`}
+                        alt=""
                     />
-                    <span
-                        role="img"
-                        aria-label="trash"
-                        onClick={() => removeFromCart(item)}
-                    >
-                        🗑️
+                </div>
+                <div>
+                    <div>{item.name}, ${item.price}</div>
+                    <div>
+                        <span>Qty:</span>
+                        <input
+                            type="number"
+                            placeholder="1"
+                            value={item.purchaseQuantity}
+                            onChange={onChange}
+                        />
+                        <span
+                            role="img"
+                            aria-label="trash"
+                            onClick={() => removeFromCart(item)}
+                        >
+                            🗑️
                     </span>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
-}
+        );
+    }
 
-export default CartItem;
+    export default CartItem;
