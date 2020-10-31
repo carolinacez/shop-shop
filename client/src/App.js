9b57e92c79e37765f2e118cd80fe0feab4e25f77
carolina <carolinaccez@gmail.com>
2020-10-31 12:26:11 -0400

    deploy2

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
 
+//import provider and store and wrap components in provider 
+
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
